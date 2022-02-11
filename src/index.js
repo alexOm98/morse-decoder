95299ff39d4dcddbe59229f4ce605cce8711c9a0
alexOm98 <95749189+alexOm98@users.noreply.github.com>
2022-02-11 18:22:41 +0300

    done

--- src/index.js
+++ src/index.js
@@ -38,8 +38,31 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let arr2 = [];
+    let arr3 = [];
+    let arr4 = [];
+    let str = "";
+    let arr = expr.match(/.{1,10}/g);
+    for (let i = 0; i < arr.length; i++) {
+      arr2.push(Number(arr[i]).toString());
+    }
+    for (let i = 0; i < arr2.length; i++) {
+      arr3.push(arr2[i].replace(/10/g, ".").replace(/11/g, "-"));
+    }
+    str = arr3.join(" ").replace(/NaN/g, " ");
+    let words = str.split("  ");
+    let symbols = words.map((char) => char.split(" "));
+    for (let i = 0; i < symbols.length; i++) {
+      arr4[i] = [];
+      for (let j = 0; j < symbols[i].length; j++) {
+        if (MORSE_TABLE[symbols[i][j]]) {
+          arr4[i].push(MORSE_TABLE[symbols[i][j]]);
+        }
+      }
+    }
+    return arr4.map((arr) => arr.join("")).join(" ");
+  }
+  
 
 module.exports = {
     decode
